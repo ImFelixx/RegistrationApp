a9b8e0641e27b6fb24fa3ed26dea0ac90d1d825a
Felix <felixng6218@gmail.com>
2025-07-15 14:12:57 +0800

    Update DB connection for deployment

--- app.js
+++ app.js
@@ -10,10 +10,11 @@ const app = express();
 
 // Database connection
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'C237_usersdb'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 3306
 });
 
 db.connect((err) => {
